83ba81207e222c706b3eb885608f49fb3b33757c
jorge19781109 <jorge.contreras1978@icloud.com>
2025-03-16 17:11:04 -0300

    Juego casi terminado
    No logro que funcione una alerte de nombre repetido, si no lo logro hoy, lo eliminaré

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado"); 
+function agregarAmigo() {
+if(inputAmigo.value ==""){
+        alert("Debes ingresar un nombre");
+    }
+
+
+    listaAmigos.push(inputAmigo.value)
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+
+
+
+function sortearAmigo(){
+    const random =Math.floor((Math.random() * listaAmigos.length));
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es ${amigoSecreto}</li>`;
+
+    //console.log(random);
+}
+    //Revisa que el nombre no este repetido, pero aun no logro que funcione
+    //if(amigo.includes(inputAmigo)){
+      //  alert(`El nombre ${inputAmigo} ya está en la lista`);
+    //}
